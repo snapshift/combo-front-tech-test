897e4b6cafa875285b0a832593eda6d0de89da0f
Clément Servillat <cservillat@gmail.com>
2023-10-27 14:41:54 +0200

    feat: ex 2

---
+++ src/exercices/exercice-2/exercice-2.test.ts
@@ -0,0 +1,20 @@
+/* eslint-disable testing-library/render-result-naming-convention */
+import { renderHook } from '@testing-library/react';
+import usePrevious from './exercice-2'
+
+describe('usePrevious', () => {
+  it('returns undefined on initial render', () => {
+    const hook = renderHook(() => usePrevious(1));
+    expect(hook.result.current).toBeUndefined();
+  });
+
+  it('returns the previous value after update', () => {
+    const hook = renderHook(({ state }) => usePrevious(state), { initialProps: { state: 1 } });
+
+    hook.rerender({ state: 2 });
+    expect(hook.result.current).toBe(1);
+
+    hook.rerender({ state: 4 });
+    expect(hook.result.current).toBe(2);
+  });
+});
--- src/exercices/exercice-2/exercice-2.tsx
+++ src/exercices/exercice-2/exercice-2.tsx
@@ -1,3 +1,23 @@
+/**
+ * Problem:
+ * Implement a custom React hook named `usePrevious`.
+ * This hook should take a value of generic type `T` as a parameter and return the previous value or `undefined` if there's none.
+ * You can assume that the value can be of any type.
+ * The test suite provided must be green. ✅
+ * 
+ * Requirements:
+ * - Use only React's built-in hooks.
+ * - The function signature should be `function usePrevious<T>(value: T): T | undefined`.
+ * 
+ * Example:
+ * const [count, setCount] = useState(0);
+ * const prevCount = usePrevious(count);
+ * // prevCount will be `undefined` initially, and then hold the previous value of `count` after updates.
+ * 
+ * Hints:
+ * - Ask for one or more if you're hard stuck !
+ */
+
 const usePrevious = () => {
 
 }
