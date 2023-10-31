ed46fb70ee04b9b64fe857ede344047e1e9bea8b
Simon Bruneaud <simon.bruneaud@gmail.com>
2023-10-31 09:19:36 +0100

    refactor: improve test 3
    Signed-off-by: Simon Bruneaud <simon.bruneaud@gmail.com>

--- README.md
+++ README.md
@@ -9,4 +9,3 @@ Start the app with:
 ```sh
 REACT_APP_EX=3 npm start
 ```
-
--- src/exercices/exercice-3/exercice-3.tsx
+++ src/exercices/exercice-3/exercice-3.tsx
@@ -1,39 +1,55 @@
 /**
- * There are some performances issues in the component.
- * Refactor it to improve performances
+ * You can start the app using:
+ * REACT_APP_EX=3 npm start
+ * 
+ * This component contains few performances issues.
+ * You can open the console to check the logs.
+ * 
+ * Try to refactor it to fix problems, and perf issues.
  */
 import React, { useState, useEffect } from "react";
 
-const fetchData = async ({ id }) => {
+const fetchData = async ({ search }): Promise<number[]> => {
   console.count("invoke:fetchData");
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve({ id, name: "x" });
+      const arr = Array.from(Array(50).keys())
+      resolve(arr);
     }, 1000);
   });
 };
 
 const UnoptimizedComponent = () => {
   console.count("render:UnoptimizedComponent");
-  const [currentId, setCurrentId] = useState<number>(0);
+  const [data, setData] = useState<number[]>([]);
+  const [search, setSearch] = useState<string>();
 
   useEffect(() => {
-    setCurrentId(1);
-  }, [setCurrentId]);
+    setSearch('');
+  }, [setSearch]);
 
   useEffect(() => {
-    fetchData({ id: currentId });
+    fetchData({search}).then((newData) => setData(newData));
   });
 
   return (
-    <div className="flex m-12 items-center gap-2">
-      <label>ID</label>
-      <input
-        type="number"
-        min="0"
-        value={currentId}
-        onChange={(e) => setCurrentId(Number(e.target.value))}
-      />
+    <div className="flex flex-col w-96 m-auto py-20 gap-10">
+      <div className="flex gap-2 items-center">
+        <label>Search</label>
+        <input
+          type="search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <div>
+      <h1 className="text-2xl mb-2">List</h1>
+        <div>
+          {data.map((item) => <div key={item} className="border-b-2 border-neutral-300 p-4">
+            <p>item n°{item}</p>
+          </div>)}
+        </div>
+      </div>
     </div>
   );
 };
